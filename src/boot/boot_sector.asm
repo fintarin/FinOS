b8c70dc143970f3e15cd3e0af8b7a0bdaa1738b5
fintarin <valukevichv@gmail.com>
2022-05-22 16:12:34 +0300

    Print string from memory

--- src/boot/boot_sector.asm
+++ src/boot/boot_sector.asm
@@ -2,13 +2,22 @@
 
 mov ah, 0x0e
 
-mov al, [the_secret]
-int 0x10
+mov bx, boot_str
+print_loop:
+  mov al, [bx]
+  cmp al, 0
+  je print_loop_end
+
+  int 0x10
+
+  add bx, 1
+  jmp print_loop
+print_loop_end:
 
 jmp $
 
-the_secret:
-  db 'X'
+boot_str:
+  db 'Booting OS', 0
 
 times 510 - ($-$$) db 0
 dw 0xaa55
